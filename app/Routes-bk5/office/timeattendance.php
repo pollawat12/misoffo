f45188dd9e2ecbe63bfdaea12a8789c9ac76cca4
ppgoro <ppgoro@gmail.com>
2023-10-05 14:04:42 +0700

    delete -- not use

--- app/Routes-bk5/office/timeattendance.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Office\Timeattendance\WorktimeController;
-
-Route::prefix('time-attendance')->namespace('Timeattendance')->group(function () {
-    // time attendance
-    Route::get('worktime', [WorktimeController::class, 'index']);
-    Route::post('check-in-time', [WorktimeController::class, 'save']);
-});
